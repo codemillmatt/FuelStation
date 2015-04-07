87ccac402ed3faf9d30f3b36083c6268ff4abd9a
Matt Soucoup <msoucoup@codemilltech.com>
2015-04-07 13:43:23 -0500

    Adding the Android implementation of sqlite platform

---
+++ Droid/AndroidSqlitePlatform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+using SQLite.Net.Interop;
+using SQLite.Net.Platform.XamarinAndroid;
+
+namespace FuelStation.Droid
+{
+	public class AndroidSqlitePlatform : IPlatform
+	{
+		public AndroidSqlitePlatform ()
+		{
+		}
+
+		public string DBPath {
+			get { 
+				return Path.Combine (Environment.GetFolderPath( Environment.SpecialFolder.Personal), "stations.db3");
+			}
+		}
+
+		public ISQLitePlatform OSPlatform {
+			get { 
+				return new SQLitePlatformAndroid ();
+			}
+		}
+	}
+}
+
